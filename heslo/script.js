5c3e758533d23412c79e332d3c1929a042d7e466
Vít Svoboda <2021117@skolasatalice.cz>
2026-06-11 16:17:19 +0200

    aktualizace

---
+++ heslo/script.js
@@ -0,0 +1,15 @@
+function zkontroluj() {
+ 
+    const heslo = document.getElementById("heslo").value;
+ 
+    if (heslo === "12") {
+ 
+        document.getElementById("obsah").style.display = "block";
+ 
+    } else {
+ 
+        alert("Špatné heslo varování varování vadne heslo varovani volam policij 🚨🚨🚨🚨🚨");
+ 
+    }
+ 
+}
